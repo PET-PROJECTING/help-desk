331b06a0e93eee70df216a57eda5cf88ed2b5a79
Oleh Klym <oleh.klym@teladochealth.com>
2023-08-20 14:34:45 +0300

    Updated App component with routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,25 @@
-import './App.css'
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {
+  NoPage,
+  Home,
+  Layout,
+  Products,
+  Customers,
+} from "./pages";
 
 function App() {
-
   return (
-    <div>Works</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />}/>
+          <Route path="customers" element={<Customers />}/>
+          <Route path="products" element={<Products />}/>
+          <Route path="*" element={<NoPage />}/>
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
