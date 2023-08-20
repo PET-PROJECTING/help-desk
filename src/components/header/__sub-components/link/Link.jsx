00fc5cb98f85076eeffb035b84b7306d642b632b
Oleh Klym <oleh.klym@teladochealth.com>
2023-08-20 14:32:25 +0300

    Added Link component and related stuff

---
+++ src/components/header/__sub-components/link/Link.jsx
@@ -0,0 +1,26 @@
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import styles from './styles.module.scss';
+import PropTypes from "prop-types";
+
+const Link = ({ to, children }) => {
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) => {
+        return classNames(styles.link, { [styles.active]: isActive } )
+      }}
+    >
+      {children}
+    </NavLink>
+  );
+};
+
+Link.displayName = 'Link';
+
+Link.propTypes = {
+  to: PropTypes.string.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Link;
---
+++ src/constants/index.js
@@ -0,0 +1,1 @@
+export * as LINKS from "./urls";
